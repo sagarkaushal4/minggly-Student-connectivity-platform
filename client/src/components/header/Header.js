8b5d007070acb6afdb054d3eba360db984b4593c
Sagar Kaushal <sagarkaushal4@gmail.com>
2023-09-14 00:47:08 +0530

    Initial commit

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+import Search from "./Search";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from '../../redux/actions/postAction';
+import { getSuggestions } from '../../redux/actions/suggestionsAction';
+
+const Header = () => {
+  const { auth } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleRefreshHome = () => {
+    window.scrollTo({top: 0})
+    dispatch(getPosts(auth.token));
+    dispatch(getSuggestions(auth.token));
+  };
+
+  return (
+    <div className="header bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+        <div className="container-fluid">
+          <Link to="/" className="logo text-decoration-none " onClick={handleRefreshHome}>
+            <h1 className="navbar-brand  p-0 m-0 "><img src="logooo.png" alt="minggly.com" /></h1>
+          </Link>
+
+          <Search />
+
+          <Menu />
+         
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
